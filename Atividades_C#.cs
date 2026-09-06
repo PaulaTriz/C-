1068bc8c9a3c55ebdde039196502f4ae7a73232f
Paula Rodrigues <rodriguespereirapaula649@gmail.com>
2026-09-06 16:51:05 -0300

    Add files via upload

---
+++ Atividades_C#.cs
@@ -0,0 +1,8 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Ola,Mundo!"); // Exibe o texto e pula para a proxima linha
+        Console.ReadLine(); // Pausa a tela esperando a tecla Enter
+    }
+}
