c5b3f90b993aa0b98da5c28ba3b9a42af8ed921d
Paula Rodrigues <rodriguespereirapaula649@gmail.com>
2026-09-08 11:02:33 -0300

    Add files via upload

---
+++ Variaveis_e_Tipos_De_Dados.cs
@@ -0,0 +1,16 @@
+﻿using System; 
+
+class Program
+{
+static void Main() 
+{
+        int idade = 25; // Quarda um número inteiro (25)
+        string nome = "João"; // Quarda um texto ("João")
+        double altura = 1.75; // Quarda um número com virgula (1.75)
+
+        Console.WriteLine("Nome: " + nome); // Exibe o texto "Nome: " seguido do valor da variavel na tela
+        Console.WriteLine("Idade: " + idade); // Exibe a idade na tela
+        Console.WriteLine("Altura: " + altura); // Exibe a altura na tela
+        Console.ReadLine();
+    }
+}
