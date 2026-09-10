e2d4b2c1c4e302ba9f3878846ef1b818970336ec
Paula Rodrigues <rodriguespereirapaula649@gmail.com>
2026-09-10 09:31:28 -0300

    Add files via upload

---
+++ Entrada_de_Dados.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite seu nome: "); // Mostra a mensagem sem pular linha para o usuario digitar do lado
+        string nome = Console.ReadLine(); // Le o texto digitado pelo usuario e salva na variavel 'nome'
+        Console.WriteLine("Olá, " + nome + "!"); // Junta as palavras e mostra a saudação personalizada
+        Console.ReadLine(); // trava o console para que a janela não feche sozinha
+    }
+}
