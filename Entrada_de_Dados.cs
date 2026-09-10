7b72a328b9772938199c64193ea33600be96fc52
Paula Rodrigues <rodriguespereirapaula649@gmail.com>
2026-09-10 09:20:57 -0300

    Add files via upload

---
+++ Entrada_de_Dados.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite sua idade: ");
+        int idade = int.Parse(Console.ReadLine());
+        if (idade >= 18)
+            Console.WriteLine("Voce é maior de idade.")
+                else(idade < 18) 
+                Console.WriteLine("Voce é menor de idade.")
+            Console.ReadLine();
+    }
+}
